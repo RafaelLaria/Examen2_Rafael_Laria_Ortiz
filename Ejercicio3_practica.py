1fc604fdbd4f92b616a338504671d2277fb6e278
RafaelLaria <rafael.lariaortiz@usp.ceu.es>
2025-11-21 11:31:38 +0100

    cambio

---
+++ Ejercicio3_practica.py
@@ -0,0 +1,9 @@
+def busqueda_secuencia(lista, objetivo):
+    for i, valor enumerate(lista):
+        if valor == objetivo:
+            return lista[i]
+        else:
+            return '-1'
+
+lista1 = [0,120,240,3406,405620,3503,4601,2502,45072404,60]
+print(busqueda_secuencia(lista1, 3503))
